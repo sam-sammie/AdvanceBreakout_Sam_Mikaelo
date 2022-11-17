0e954d0bfb058579c7033869de650d11e1c09010
sam-sammie <isabella7675@gmail.com>
2022-11-17 12:09:10 -0500

    AdvanceBreakout ver[0.0.0.0]

---
+++ SDL_Tut/AnimatedTexture.h
@@ -0,0 +1,41 @@
+#ifndef _ANIMATEDTEXTURE_H
+#define _ANIMATEDTEXTURE_H
+#include "Texture.h"
+#include "Timer.h"
+
+namespace SDLFramework {
+	class AnimatedTexture : public Texture 
+	{
+	public:
+		enum WrapMode { Once = 0, Loop = 1 };
+		enum AnimDir { Horizontal = 0, Vertical = 1 };
+
+	private:
+		Timer* m_pTimer;
+		int mStartX;
+		int mStartY;
+		int mFrameCount; // total frames
+		float mAnimationSpeed; // in seconds
+		float mTimerPerFrame; // time to display
+		float mAnimationTimer; // time displayed
+
+		WrapMode mWrapMode;
+		AnimDir mAnimationDirection;
+
+		bool mAnimationDone;
+
+	public:
+		AnimatedTexture(std::string filename, int x, int y, int w, int h, int frameCount, float animationSpeed,
+			AnimDir animationDir, bool managed = false);
+		~AnimatedTexture();
+
+		void SetWrapMode(WrapMode mode);
+		void ResetAnimation();
+		bool IsAnimating();
+
+		void Update();
+	};
+}
+
+
+#endif
